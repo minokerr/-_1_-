4c22681672f42ee827c8617a92cc648299ea2240
minokerr <162557365+minokerr@users.noreply.github.com>
2024-10-22 15:06:35 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
+
+
